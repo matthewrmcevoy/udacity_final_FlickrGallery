74a1aeba87886bcf8a1a2220d42e7ae647afc070
matthewrmcevoy <matthewrmcevoy@gmail.com>
2022-10-24 15:09:32 -0400

    Commit 17 - added onClick view full image on userPhotos view

--- app/src/main/java/com/mrm/android/flikrtest/ui/userphotos/UserPhotosFragment.kt
+++ app/src/main/java/com/mrm/android/flikrtest/ui/userphotos/UserPhotosFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.mrm.android.flikrtest.R
 import com.mrm.android.flikrtest.databinding.FragmentUserPhotosBinding
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_user_photos.*
 
 class UserPhotosFragment : Fragment() {
 
@@ -23,9 +25,18 @@ class UserPhotosFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
         binding.userPhotoGrid.adapter = UserPhotoGridAdapter(UserPhotoGridAdapter.OnClickListener{
-            null
+            showFullPhoto(it)
         })
         return binding.root
+    }
+
+    fun showFullPhoto(userPhoto: UserPhoto){
+        val url = userPhoto.photoUrl
+        Picasso.get().load(url).into(user_detail_view)
+        user_detail_view.visibility=View.VISIBLE
+        user_detail_view.setOnClickListener {
+            user_detail_view.visibility = View.GONE
+        }
     }
 
 }
